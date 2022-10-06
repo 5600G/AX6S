d30ccf4eb45d6886d30f6ec5c8db8a307c78578e
5600G <114322350+5600G@users.noreply.github.com>
2022-10-07 04:19:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,15 +19,4 @@
 
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'CONFIG_PACKAGE_luci-app-ssr-plus=y' >>.config
-echo 'CONFIG_DEFAULT_luci-app-unblockmusic=n' >>.config
-echo 'CONFIG_PACKAGE_luci-app-adblock=n' >>.config
-echo 'CONFIG_PACKAGE_adguardhome=y' >>.config
-echo 'CONFIG_PACKAGE_rclone=y' >>.config
-echo 'CONFIG_PACKAGE_rclone-ng=y' >>.config
-echo 'CONFIG_PACKAGE_aria2=y' >>.config
-echo 'CONFIG_PACKAGE_ariang=y' >>.config
-echo 'CONFIG_PACKAGE_apfree-wifidog=y' >>.config
-echo 'CONFIG_PACKAGE_aircrack-ng=y' >>.config
-echo 'CONFIG_PACKAGE_reaver=y' >>.config
-echo 'CONFIG_PACKAGE_frpc=y' >>.config
+
